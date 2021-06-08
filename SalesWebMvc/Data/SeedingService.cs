9467b1601cba98d7fd081360e8266628a6ac0e87
Lucas Santana <lucas_377@hotmail.com>
2021-06-08 10:52:40 -0300

    Seeding service, Sellers controller and view

---
+++ SalesWebMvc/Data/SeedingService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using SalesWebMvc.Models;
+using SalesWebMvc.Models.Enums;
+
+namespace SalesWebMvc.Data
+{
+    public class SeedingService
+    {
+        private SalesWebMvcContext _context;
+
+        public SeedingService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public void Seed()
+        {
+            if (_context.Department.Any() ||
+                _context.SalesRecord.Any() ||
+                _context.Seller.Any())
+            {
+                return; // DB already seeded.
+            }
+
+            // Instantiate base objects.
+            Department d1 = new Department(1, "Departamento 1");
+            Department d2 = new Department(2, "Departamento 2");
+
+            Seller s1 = new Seller(1, "Lucas", "lucas@mps.com.br", new DateTime(1992, 11, 01), 1700.00, d1);
+
+            SalesRecord sr1 = new SalesRecord(1, new DateTime(2021, 06, 08), 500.36, SaleStatus.Billed, s1);
+
+            // Push to database.
+            _context.Department.AddRange(d1, d2);
+            _context.Seller.Add(s1);
+            _context.SalesRecord.Add(sr1);
+
+            _context.SaveChanges();
+        }
+    }
+}
--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -1,16 +1,12 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc
 {
@@ -36,17 +32,23 @@ namespace SalesWebMvc
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
+            // Connect app to database.
             services.AddDbContext<SalesWebMvcContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), 
                     builder => builder.MigrationsAssembly("SalesWebMvc")));
+
+            // Register service in the dependency injection system.
+            services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
